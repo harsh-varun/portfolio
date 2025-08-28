7dcaf77209dcfeac7154e83191a06b79e9eddee7
Harsh Varun <harsh.varun01@gmail.com>
2025-08-28 16:02:18 +0530

    Updates

---
+++ _next/static/chunks/a8cd1fa249149470.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/b69d9149db11d7c0.js",
+  "static/chunks/turbopack-f87daad58c246091.js"
+])
---
+++ _next/static/chunks/f520624fb4f7b82e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/b69d9149db11d7c0.js",
+  "static/chunks/turbopack-fe3dae423fb33b73.js"
+])
